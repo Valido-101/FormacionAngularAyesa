55ee7c1e43f270a8cf6d53d60d763760900e96aa
jesus valido zafra <jesuszafra22@gmail.com>
2022-01-24 18:01:58 +0100

    Componente nuevo añadido al routing app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExpositorLibrosComponent } from './expositor-libros/expositor-libros.component';
 
-const routes: Routes = [];
+//Rutas a través de las que se accede al componente desde la aplicación
+const routes: Routes = [
+  {
+    path: 'expositor',
+    component: ExpositorLibrosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

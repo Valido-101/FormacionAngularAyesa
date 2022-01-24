87388d352cb468d87297f031a2b015b8aa8478da
jesus valido zafra <jesuszafra22@gmail.com>
2022-01-24 17:57:38 +0100

    Componente expositorLibros creado

---
+++ src/app/expositor-libros/expositor-libros.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-expositor-libros',
+  templateUrl: './expositor-libros.component.html',
+  styleUrls: ['./expositor-libros.component.css']
+})
+export class ExpositorLibrosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

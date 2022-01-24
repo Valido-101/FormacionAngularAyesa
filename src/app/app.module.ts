5f6d189d73defe2e09ca770d2011102435710557
jesus valido zafra <jesuszafra22@gmail.com>
2022-01-24 17:58:39 +0100

    Archivos olvidados en el commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExpositorLibrosComponent } from './expositor-libros/expositor-libros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExpositorLibrosComponent
   ],
   imports: [
     BrowserModule,

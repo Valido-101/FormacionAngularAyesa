eb1f32791c843b2e74fcf02d87e18d3dd82933d9
jesus valido zafra <jesuszafra22@gmail.com>
2022-01-25 14:38:17 +0100

    Creados componentes login y creacionLibro, guards auth y authed y expositorLibros reubicado al directorio de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ExpositorLibrosComponent } from './expositor-libros/expositor-libros.component';
+import { ExpositorLibrosComponent } from './components/expositor-libros/expositor-libros.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import { LoginComponent } from './components/login/login.component';
+import { CreacionLibroComponent } from './components/creacion-libro/creacion-libro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExpositorLibrosComponent
+    ExpositorLibrosComponent,
+    LoginComponent,
+    CreacionLibroComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/creacion-libro/creacion-libro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-creacion-libro',
+  templateUrl: './creacion-libro.component.html',
+  styleUrls: ['./creacion-libro.component.css']
+})
+export class CreacionLibroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
